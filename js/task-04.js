81a4c4bfe7f067d0d33302cf85f264733c8fabe0
Yaroslava Yemets <yaroslava1058@gmail.com>
2021-05-22 01:41:35 +0300

    task 4

--- js/task-04.js
+++ js/task-04.js
@@ -11,3 +11,32 @@
 // Добавь слушатели кликов на кнопки, вызовы
 //  функций и обновление интерфейса
 
+
+const counter = {
+    value: 0,
+    increment() {
+        console.log(this);
+        this.value += 1;
+    },
+
+    decrement() {
+        console.log(this);
+        this.value -= 1;
+    }
+};
+
+
+const counterValue = document.querySelector('#value');
+const decrementBtn = document.querySelector('[data-action="decrement"]');
+const incrementBtn = document.querySelector('[data-action="increment"]');
+
+decrementBtn.addEventListener('click', function () {
+    counter.decrement();
+    counterValue.textContent = counter.value;
+});
+
+
+incrementBtn.addEventListener('click', function () {
+    counter.increment();
+    counterValue.textContent = counter.value;
+});
