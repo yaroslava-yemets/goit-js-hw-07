f6ee5567b4147233b3e74a22a525f62608f09c5b
Yaroslava Yemets <yaroslava1058@gmail.com>
2021-05-22 16:13:30 +0300

    done task 7, and amended task 6

--- js/task-06.js
+++ js/task-06.js
@@ -10,17 +10,20 @@
 // если неправильное - красным.
 
 const inputEl = document.querySelector('#validation-input');
+const wornedTextEl = document.querySelector('#worned-text'); 
 
 inputEl.addEventListener('blur', checkInputLength);
 
 function checkInputLength () {
     if (inputEl.value.length === Number(inputEl.dataset.length)) {
         inputEl.classList.add('valid') 
+        wornedTextEl.textContent = 'Вы ввели правильное количество символов';
         if (inputEl.classList.contains('invalid')) {
             inputEl.classList.remove('invalid');
         };
     } if (inputEl.value.length !== Number(inputEl.dataset.length)) {
         inputEl.classList.add('invalid');
+        wornedTextEl.textContent = 'Вы ввели неправильное количество символов';
         if (inputEl.classList.contains('valid')) {
             inputEl.classList.remove('valid');
         };
